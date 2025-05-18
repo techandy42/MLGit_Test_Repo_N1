f7a6a878627d4378db6508cbbc69600f4fbdab00
Andy Lee <andylee@Mac.attlocal.net>
2025-05-18 00:06:18 -0700

    fixed naming typo

--- src/subdirectory_1/inner_module_consumer_a.py
+++ src/subdirectory_1/inner_module_consumer_a.py
@@ -2,11 +2,11 @@ import sys
 import os
 abspath = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(abspath)
-from outer_module_producer_a import omca_dummy_producer_1
+from outer_module_producer_a import ompa_dummy_producer_1
 sys.path.remove(abspath)
 
 def imca_dummy_consumer_1():
-    omca_dummy_producer_1()
+    ompa_dummy_producer_1()
     print("inner_module_consumer_a.imca_dummy_consumer_1()")
 
 if __name__ == "__main__":
